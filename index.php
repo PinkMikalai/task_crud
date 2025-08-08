2b6af8d573a70e8b715adba65ed4c658081fb867
PinkMikalai <job.m.sashko@gmail.com>
2025-08-08 14:26:17 +0200

    debut d index

--- index.php
+++ index.php
@@ -11,15 +11,24 @@ echo "test";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Task Crud By Mikalai</title>
-    <link rel="stylesheet" href="assrts/style/style.css">
+    <link rel="stylesheet" href="assets/style/style.css">
 </head>
 
 <body>
     <?= include "assets/includes/header.html"; ?>
     <main>
         <section>
+            <h2>
+                ici l endroit ou je em trouve
+            </h2>
+            <div>
+                <h3>
+                    ici title
+                </h3>
+            </div>
             <article>
-
+                <p>ici discription</p>
+                <span>ici status</span>
             </article>
         </section>
     </main>
