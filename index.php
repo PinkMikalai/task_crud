3caed015745d93540789426a47f403d2fe4ce2ca
PinkMikalai <job.m.sashko@gmail.com>
2025-08-08 14:14:00 +0200

    l archetecture

--- index.php
+++ index.php
@@ -1,3 +1,29 @@
 <?php
 echo "test";
 ?>
+
+
+<!-- ici ma page principale -->
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Task Crud By Mikalai</title>
+    <link rel="stylesheet" href="assrts/style/style.css">
+</head>
+
+<body>
+    <?= include "assets/includes/header.html"; ?>
+    <main>
+        <section>
+            <article>
+
+            </article>
+        </section>
+    </main>
+    <?= include "assets/includes/footer.html"; ?>
+</body>
+
+</html>
